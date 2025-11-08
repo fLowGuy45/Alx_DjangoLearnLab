cc60d124eba266f7dbfbffe6530d67ed63fc9a15
Othniel <azureoniel19@gmail.com>
2025-11-08 02:16:59 +0000

    Fixed missing Django auth imports and implemented authentication views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import DetailView
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Book, Library
 
 # Function-based view to list all books
@@ -7,15 +9,16 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view to show library details
+# Class-based view for library details
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.shortcuts import redirect
+
+# ---------------------------
+# AUTHENTICATION VIEWS
+# ---------------------------
 
 # User registration
 def register_view(request):
