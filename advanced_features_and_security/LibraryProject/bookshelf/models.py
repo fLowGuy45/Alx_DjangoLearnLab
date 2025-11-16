ada3d4e1a879cd15579d6ece6b80a6737b8aaffe
Othniel <azureoniel19@gmail.com>
2025-11-16 21:30:39 +0000

    Added custom permissions and enforced them in views

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,7 +1,26 @@
+# models.py
+# Custom permissions for Book model: can_view, can_create, can_edit, can_delete
 
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.db import models
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,46 @@
+# views.py
+# Permissions enforced using @permission_required decorator:
+# - can_view: allows viewing list of books
+# - can_create: allows creating new books
+# - can_edit: allows editing existing books
+# - can_delete: allows deleting books
+
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book
+
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def view_books(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        author = request.POST['author']
+        published_date = request.POST['published_date']
+        Book.objects.create(title=title, author=author, published_date=published_date)
+        return redirect('view_books')
+    return render(request, 'bookshelf/book_form.html')
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    if request.method == "POST":
+        book.title = request.POST['title']
+        book.author = request.POST['author']
+        book.published_date = request.POST['published_date']
+        book.save()
+        return redirect('view_books')
+    return render(request, 'bookshelf/book_form.html', {'book': book})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    book.delete()
+    return redirect('view_books')
 
 # Create your views here.
