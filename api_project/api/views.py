365be485b6797db24d22cd49fcac25a6173436ea
Othniel <azureoniel19@gmail.com>
2025-11-19 02:06:14 +0000

    Added BookViewSet and DRF router for full CRUD operations

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,3 +5,16 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+from rest_framework import generics, viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
