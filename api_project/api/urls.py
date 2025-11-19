874442da8d986cdea00c39e7e86fed894ee78ef3
Othniel <azureoniel19@gmail.com>
2025-11-19 02:19:04 +0000

    Added token authentication, permission classes, and authentication endpoint

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,3 +1,4 @@
+from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookList, BookViewSet
@@ -8,6 +9,7 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     # Existing ListAPIView endpoint
     path('books/', BookList.as_view(), name='book-list'),
+path('get-token/', obtain_auth_token, name='get-token'),
 
     # Router-generated CRUD endpoints
     path('', include(router.urls)),
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -18,3 +19,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+permission_classes = [IsAuthenticated]
