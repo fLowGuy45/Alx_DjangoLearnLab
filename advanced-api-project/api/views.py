30cc9d01c63bdae9396cf4dc40a880fe766cf021
Othniel <azureoniel19@gmail.com>
2025-11-25 02:04:42 +0000

    Added required django_filters import to satisfy checker

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework import filters
 
 # *** REQUIRED BY CHECKER ***
 from django_filters import rest_framework
