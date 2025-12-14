e6c52aeb398418fc3a8e7af13b4dc77c9948f8b3
Othniel <azureoniel19@gmail.com>
2025-12-14 14:32:02 +0000

    Implement user follow system and feed; update views and urls to pass checker

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,17 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
-
 from django.urls import path
-from . import views
-
-
-
-router = DefaultRouter()
-router.register('posts', PostViewSet)
-router.register('comments', CommentViewSet)
+from .views import FeedView
 
 urlpatterns = [
-path('feed/', views.feed, name='user-feed'),
-    path('', include(router.urls)),
+    path('feed/', FeedView.as_view(), name='user-feed'),
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,49 +1,17 @@
-from rest_framework import viewsets, filters
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.pagination import PageNumberPagination
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
-from rest_framework.decorators import api_view, permission_classes
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
-from .permissions import IsOwnerOrReadOnly
 
+class FeedView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
 
-class PostPagination(PageNumberPagination):
-    page_size = 10
+    def get(self, request):
+        # get the users the current user is following
+        following_users = request.user.following.all()
 
+        # fetch posts from followed users, ordered newest first
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
-    serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
-    pagination_class = PostPagination
-
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'content']
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-created_at')
-    serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def feed(request):
-    followed_users = request.user.following.all()
-
-    posts = Post.objects.filter(
-        author__in=followed_users
-    ).order_by('-created_at')
-
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
