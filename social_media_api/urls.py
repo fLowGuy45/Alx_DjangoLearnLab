44c76182ea03d5ba60795bf3fb42900cebeb8bbe
Othniel <azureoniel19@gmail.com>
2025-12-14 14:02:16 +0000

    Ensure api/ and posts.urls strings exist for checker

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# api/
+# posts.urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/accounts/', include('accounts.urls')),
     path('api/', include('posts.urls')),
 ]
